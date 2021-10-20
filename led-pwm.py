cfc5f7342560e5a5d669659441194aed68a408f1
Nicholas Vece <91498866+razdor99@users.noreply.github.com>
2021-10-20 01:44:09 +0000

    d

--- led-pwm.py
+++ led-pwm.py
@@ -8,7 +8,7 @@ with open('led-pwm.txt', 'w') as f:
 
 print('Content-type: text/html\n\n')
 print('<html>')
-print('<form action="/cgi-bin/led_pwm.py" method="POST">')
+print('<form action="/cgi-bin/led-pwm.py" method="POST">')
 print('<input type="range" name="slider1" min="0" max="100" value="%s"><br>' % s1)
 print('<input type="submit" value="Change LED brightness">')
 print('</form>')
